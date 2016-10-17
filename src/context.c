40f92cdaa5a8d7264947c675545ca8f2e7b1303b
edvardsp <edvardsp@github.com>
2016-10-17 15:12:21 +0100

    staging code for implementation of new context setup

--- src/context.c
+++ src/context.c
@@ -8,6 +8,17 @@
 #include "debug.h"
 #include "internal.h"
 
+/* static */
+/* void _context_threadfxn(Context *ctx) */
+/* { */
+/*     ASSERT_NOTNULL(ctx); */
+/*     /1* do scheduler stuff *1/ */
+/*     /1* call ctx->fxn() *1/ */
+/*     /1* when fxn returns, do *1/ */
+/*         /1* maybe clean up context? *1/ */
+/*         /1* return control to scheduler *1/ */
+/* } */
+
 int context_create(Context **new_ctx, CtxFxn fxn)
 {
     ASSERT_NOTNULL(new_ctx);
@@ -25,6 +36,7 @@ int context_create(Context **new_ctx, CtxFxn fxn)
         PERROR("malloc failed for Context stack\n");
         return errno;
     }
+
     /* if (posix_memalign(&ctx->stack, (size_t)getpagesize(), sched->stack_size)) { */
     /*     PERROR("posix_memalign failed\n"); */
     /*     return errno; */
