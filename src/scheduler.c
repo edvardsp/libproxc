954c905f36866e14220838125838e33bdb98d1a6
edvardsp <edvardsp@github.com>
2016-10-17 15:11:40 +0100

    removed old unused functions

--- src/scheduler.c
+++ src/scheduler.c
@@ -15,10 +15,10 @@
 pthread_key_t g_key_sched;
 static pthread_once_t g_key_once = PTHREAD_ONCE_INIT;
 
-static pthread_t *g_kernel_threads = NULL;
-static Scheduler *g_scheds = NULL;
+/* static pthread_t *g_kernel_threads = NULL; */
+/* static Scheduler *g_scheds = NULL; */
 
-static unsigned long g_num_cores = 0;
+/* static unsigned long g_num_cores = 0; */
 
 static
 void _scheduler_key_create(void)
@@ -74,124 +74,8 @@ int scheduler_run(void)
 
         sched->curr_ctx = curr_ctx;
         PDEBUG("This is from scheduler!\n");
+        PDEBUG("ptr %p\n", (void *)curr_ctx->ctx.uc_link);
         ASSERT_0(scheduler_switch(&sched->ctx, &curr_ctx->ctx));
-    }
-
-    return 0;
-}
-
-void* _scheduler_reschedule()
-{
-
-    return NULL;
-}
-
-void* _scheduler_mainloop(void *arg)
-{
-    (void)arg;
-
-    Scheduler *sched;
-    ASSERT_0(
-        scheduler_create(&sched)
-    );
-
-    PDEBUG("mainloop started\n");
-    Context *new_context;
-    for (;;) {
-        // Find next contex to switch to
-        new_context = TAILQ_LAST(&sched->readyQ, ContextQ);
-        /* FIXME */
-        if (new_context == NULL) continue;
-        sched->curr_ctx = new_context;
-        PDEBUG("This is from scheduler!\n");
-        ASSERT_0(scheduler_switch(&sched->ctx, &new_context->ctx));
-    }
-
-    return NULL;
-}
-
-int _scheduler_detect_cores(void)
-{
-    long nprocessors_onln = sysconf(_SC_NPROCESSORS_ONLN);
-    if (nprocessors_onln == -1) {
-        PERROR("sysconf failed\n");
-        return errno;
-    }
-    ASSERT_TRUE(nprocessors_onln > 0);
-    g_num_cores = (unsigned long)nprocessors_onln;
-    PDEBUG("%lu cores detected!\n", g_num_cores);
-    return 0;
-}
-
-int _scheduler_start_multicore(void)
-{
-    if ((g_kernel_threads = malloc(sizeof(pthread_t) * g_num_cores)) == NULL) {
-        PERROR("malloc failed for g_kernel_threads\n");
-        return errno;
-    }
-
-    if ((g_scheds = malloc(sizeof(Scheduler) * g_num_cores)) == NULL) {
-        PERROR("malloc failed for g_schedulers\n");
-        return errno;
-    }
-    
-    // main thread is one of the pthreads
-    g_kernel_threads[0] = pthread_self();
-
-    // create rest of the pthreads
-    for (unsigned long i = 1; i < g_num_cores; i++) {
-        pthread_t *pt = &g_kernel_threads[i];
-        ASSERT_0(pthread_create(pt, NULL, _scheduler_mainloop, (void *)i));
-    }
-
-    // specify core affinity for each pthread
-    cpu_set_t cpuset;
-    for (unsigned long i = 0; i < g_num_cores; i++) {
-        pthread_t pt = g_kernel_threads[i];
-        CPU_ZERO(&cpuset);
-        CPU_SET(i, &cpuset);
-        ASSERT_0(
-            pthread_setaffinity_np(pt, sizeof(cpuset), &cpuset)
-        );
-    }
-
-    return 0;
-}
-
-int _scheduler_start_singlecore(void)
-{
-    if ((g_kernel_threads = malloc(sizeof(pthread_t))) == NULL) {
-        PERROR("malloc failed for g_kernel_threads\n");
-        return errno;
-    }
-
-    if ((g_scheds = malloc(sizeof(Scheduler))) == NULL) {
-        PERROR("malloc failed for g_schedulers\n");
-        return errno;
-    }
-
-    // main thread is only pthread
-    g_kernel_threads[0] = pthread_self();
-
-    return 0;
-}
-
-int _scheduler_start(void)
-{
-    _scheduler_key_create();
-    
-    int ret;
-    // set to multicore
-    if (g_num_cores != 0) {
-        if ((ret = _scheduler_start_multicore()) != 0) {
-            return ret;
-        } 
-    }
-    // or single core
-    else {
-        if ((ret = _scheduler_start_singlecore()) != 0) {
-            return ret;
-        }
     }
 
     return 0;
