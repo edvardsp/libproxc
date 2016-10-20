eaaf9d79335cac6f4061ae4009c42098b9eaffe3
edvardsp <edvardsp@github.com>
2016-10-20 15:39:33 +0100

    Added macro disabling

--- src/proxc.h
+++ src/proxc.h
@@ -10,10 +10,14 @@ void proxc_end(void);
 void* proxc_proc(ProcFxn, ...);
 int proxc_par(int, ...);
 
-#define PROC(...)  proxc_proc(__VA_ARGS__, NULL)
-#define PAR(...)   proxc_par(0, __VA_ARGS__, NULL)
-
 void proc_yield(void);
+
+#ifndef PROXC_NO_MACRO
+
+#   define PROC(...)  proxc_proc(__VA_ARGS__, NULL)
+#   define PAR(...)   proxc_par(0, __VA_ARGS__, NULL)
+
+#endif /* PROXC_NO_MACRO */
 
 #endif /* PROXC_H__ */
 
