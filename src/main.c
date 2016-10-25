6110dabf92ea2f58a995e03188d0b17c7f90664d
edvardsp <edvardsp@github.com>
2016-10-25 13:44:21 +0100

    chan demo and some simple benchmarking

--- src/main.c
+++ src/main.c
@@ -1,71 +1,92 @@
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
 
 #include "proxc.h"
 
 void fxn1(void) 
 { 
-    Chan *ch1 = ARGN(0);
-    printf("fxn1: Hello from this side!\n");
-    for (int i = 0; i < 4; i++) {
-        int value = i * 11;
-        printf("fxn1: to chan, %d\n", value);
-        chan_write(ch1, &value, sizeof(int));
+    Chan *a = ARGN(0), *b = ARGN(1);
+    int value = 0;
+    chan_write(a, &value, sizeof(int));
+    int running = 1;
+    while (running) {
+        chan_read(b, &value, sizeof(int));
+        chan_write(a, &value, sizeof(int));
     }
 }
 void fxn2(void) 
 { 
-    Chan *ch1 = ARGN(0);
-    if (ch1) {
-        printf("fxn2: got CHAN!\n");
-        for (int i = 0; i < 4; i++) {
-            int value;
-            chan_read(ch1, &value, sizeof(int));
-            printf("fxn2: from chan, %d\n", value);
-        }
-    }
-    else {
-        printf("fxn2: no CHAN...\n");
-        for (int i = 2; i < 7; i++) {
-            printf("fxn2: %d\n", i);
-            proc_yield();
-        }
+    Chan *a = ARGN(0), *c = ARGN(1), *d = ARGN(2);
+    int value;
+    int running = 1;
+    while (running) {
+        chan_read(a, &value, sizeof(int));
+        chan_write(d, &value, sizeof(int));
+        chan_write(c, &value, sizeof(int));
     }
 }
 
 void fxn3(void)
 {
-    printf("fxn3: Now it is my turn!\n");
-    for (int i = 0; i < 10; i++) {
-        printf("fxn3: %d\n", i);
+    Chan *b = ARGN(0), *c = ARGN(1);
+    int value;
+    int running = 1;
+    while (running) {
+        chan_read(c, &value, sizeof(int));
+        value++;
+        chan_write(b, &value, sizeof(int));
+    }
+}
+
+__attribute((noreturn))
+void timerFxn(void)
+{
+    Chan *d = ARGN(0);
+    int repeat = 5000;
+    int runs = 30;
+    clock_t start, stop;
+    double time_spent, sum;
+
+    sum = 0;
+    for (int j = 0; j < runs; j++) {
+        start = clock();
+
+        int x;
+        for (int i = 0; i < repeat; i++ ) {
+            chan_read(d, &x, sizeof(int));
+        }
+        stop = clock();
+        time_spent = (double)(stop - start) / CLOCKS_PER_SEC * 1000.0;
+        sum += time_spent;
+        printf("\t%f\n", time_spent);
     }
 
-    printf("fxn3: PAR start\n");
-    Chan *ch2;
-    chan_create(&ch2);
-    PAR(
-        PROC(fxn2),
-        PROC(fxn1, ch2),
-        PROC(fxn2, ch2)
-    );
-    chan_free(ch2);
-    printf("fxn3: PAR ended\n");
+    printf("Average = %f ms/iteration\n", (sum) / (runs));
+    exit(0);
 }
 
 void foofunc(void)
 {
     printf("foofunc: PAR start\n");
 
-    Chan *ch1;
-    chan_create(&ch1);
+    Chan *a, *b, *c, *d;
+    chan_create(&a);
+    chan_create(&b);
+    chan_create(&c);
+    chan_create(&d);
     PAR(
-        PROC(fxn1, ch1),
-        PROC(fxn2, ch1),
-        PROC(fxn3),
-        PROC(fxn2)
+        PROC(fxn1, a, b),
+        PROC(fxn2, a, c, d),
+        PROC(fxn3, b, c),
+        PROC(timerFxn, d)
     );
-    chan_free(ch1);
+    chan_free(a);
+    chan_free(b);
+    chan_free(c);
+    chan_free(d);
 
     printf("foofunc: PAR ended\n");
 }
