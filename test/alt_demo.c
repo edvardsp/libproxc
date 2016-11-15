23d395df0a45a30bbd1701a835d007f562342951
edvardsp <edvardsp@github.com>
2016-11-15 17:46:40 +0000

    fixed alt_demo

--- test/alt_demo.c
+++ test/alt_demo.c
@@ -6,88 +6,47 @@
 
 #include "proxc.h"
 
-void fxn1(void) 
+void worker(void) 
 { 
-    printf("fxn1: start\n");
-
-    Chan *ch = ARGN(0);
-    ChanEnd *ch_end = chan_getend(ch);
+    Chan *chint = ARGN(0);
+    int id = *(int *)ARGN(1);
     
-    int running = 1;
-    while (running) {
-        int value = 42;
-        printf("fxn1: trying to write %d\n", value);
-        chan_write(ch_end, &value, sizeof(int));
-        printf("fxn1: succeded\n");
-    }
-
-    printf("fxn1: stop\n");
-}
-
-void fxn2(void) 
-{ 
-    printf("fxn2: start\n");
-
-    Chan *ch = ARGN(0);
-    ChanEnd *ch_end = chan_getend(ch);
+    printf("worker %d: start\n", id);
 
     int running = 1;
+    int value = id * 3;
     while (running) {
-        int value = 666;
-        printf("fxn2: trying to write %d\n", value);
-        chan_write(ch_end, &value, sizeof(int));
-        printf("fxn2: succeded\n");
+        printf("worker %d: trying to write %d\n", id, value);
+        CHWRITE(chint, &value, int);
+        printf("worker %d: succeded\n", id);
     }
 
-    printf("fxn2: stop\n");
-}
-
-void fxn3(void)
-{
-    printf("fxn3: start\n");
-
-    Chan *ch = ARGN(0);
-    ChanEnd *ch_end = chan_getend(ch);
-
-    int running = 1;
-    while (running) {
-        int value = 1337;
-        printf("fxn3: trying to write %d\n", value);
-        chan_write(ch_end, &value, sizeof(int));
-        printf("fxn3: succeded\n");
-    }
-
-    printf("fxn3: stop\n");
+    printf("worker %d: stop\n", id);
 }
 
 __attribute__((noreturn))
 void foofunc(void)
 {
-    printf("foofunc: PAR start\n");
+    printf("foofunc: start\n");
 
-    #define NUM_CHS 3
-    Chan *chs[NUM_CHS];
-    ChanEnd *ch_ends[NUM_CHS];
-    for (int i = 0; i < NUM_CHS; i++) {
-        chs[i] = chan_create();
-        ch_ends[i] = chan_getend(chs[i]);  
+    enum { NUM_WORKERS = 3 };
+    Chan *chs[NUM_WORKERS];
+    int ids[NUM_WORKERS];
+    for (int i = 0; i < NUM_WORKERS; i++) {
+        chs[i] = CHOPEN(int);
+        ids[i] = i;
+        GO(PROC(worker, chs[i], &ids[i]));
     }
-
-    GO(PAR(
-           PROC(fxn3, chs[2]),
-           PROC(fxn2, chs[1]),
-           PROC(fxn1, chs[0])
-       )
-    );
+    YIELD();
 
     int x = 2, y = 4;
     int val1, val2, val3;
     for (int i = 0; i < 10; i++) {
         printf("foofunc: round %d\n", i);
         switch(ALT(
-            GUARD(i  < y, ch_ends[0], &val1, int),
-            GUARD(i  > x, ch_ends[1], &val2, int),
-            GUARD(1, ch_ends[2], &val3, int)
+            GUARD(i  < y, chs[0], &val1, int),
+            GUARD(i  > x, chs[1], &val2, int),
+            GUARD(     1, chs[2], &val3, int)
         )) {
         case 0: // guard 0
             printf("\tguard 0 accepted, %d\n", val1);
@@ -101,10 +60,10 @@ void foofunc(void)
         }
     }
 
-    for (int i = 0; i < NUM_CHS; i++)
-        chan_free(chs[i]);
+    for (int i = 0; i < NUM_WORKERS; i++)
+        CHCLOSE(chs[i]);
 
-    printf("foofunc: PAR ended\n");
+    printf("foofunc: stop\n");
     exit(0);
 }
 
