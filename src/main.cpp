7919486b9e634144c31957fa1a9a10545b315615
edvardsp <edvardsp@github.com>
2017-02-20 14:48:27 +0100

    Added include directory, and some dummy files.

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream>
+
+#include <proxc/proxc.hpp>
+
+int main()
+{
+    std::cout << get_name() << std::endl;
+    return 0;
+}
---
+++ src/proxc.cpp
@@ -0,0 +1,12 @@
+
+#include <string>
+
+#include "proxc/main.hpp"
+
+static constexpr const char *NAME = "ProXC - A modern concurrency library";
+
+std::string get_name() 
+{
+    return NAME;
+}
+
