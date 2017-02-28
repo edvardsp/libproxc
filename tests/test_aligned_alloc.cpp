cb39c08a84c78abf06040762a501cdfc0ff9d1ce
edvardsp <edvardsp@github.com>
2017-02-28 12:03:06 +0100

    Changed some asserts to assert_eq

--- tests/test_aligned_alloc.cpp
+++ tests/test_aligned_alloc.cpp
@@ -12,10 +12,10 @@ void test_aligned_alloc()
     const std::size_t align = 1024;
 
     void *ptr = proxc::aligned_alloc(4096, align);
-    throw_assert(ptr, "aligned_alloc returned nullptr");
+    throw_assert(!!ptr, "aligned_alloc returned nullptr");
 
     auto uptr = reinterpret_cast<std::uintptr_t>(ptr);
-    throw_assert((uptr % static_cast<std::uintptr_t>(align)) == 0, "ptr `" << ptr << "` is not aligned `" << align << "`");
+    throw_assert_eq(uptr % static_cast<std::uintptr_t>(align), 0u, "ptr is not aligned");
 
     proxc::aligned_free(ptr);
 }
@@ -27,30 +27,30 @@ void test_aligned_array()
     
     {
         proxc::AlignedArray<int, align> array{};
-        throw_assert(array.size() == 0, "array length is not zero: " << array.size());
-        throw_assert(array.get() == nullptr, "array ptr is not nullptr: " << array.get());
+        throw_assert_eq(array.size(), 0u, "array length is not zero");
+        throw_assert(array.get() == nullptr, "array ptr is not nullptr");
         throw_assert(!array, "array bool operator does not equal false");
     }
 
     {
         proxc::AlignedArray<int, align> array{nullptr};
-        throw_assert(array.size() == 0, "array length is not zero: " << array.size());
-        throw_assert(array.get() == nullptr, "array ptr is not nullptr: " << array.get());
+        throw_assert_eq(array.size(), 0u, "array length is not zero");
+        throw_assert(array.get() == nullptr, "array ptr is not nullptr");
         throw_assert(!array, "array bool operator does not equal false");
     }
 
     {
         proxc::AlignedArray<int, align> array{size};
-        throw_assert(array.size() == size, "array size does not match size " << size);
+        throw_assert_eq(array.size(), size, "array size does not match size");
         throw_assert(array.get() != nullptr, "array ptr is nullptr");
         throw_assert(array, "array bool operator does not equal true");
         
         const std::size_t index = size % 10;
-        const std::size_t value = 42;
-        throw_assert(array[index] == int{}, "default array value `" << array[index] << "` is not equal default constructor value `" << int{} << "`");
+        const auto value = 42;
+        throw_assert_eq(array[index], int{}, "default array value is not equal to default constructor value");
         array[index] = value;
-        throw_assert(array[index] == value, "yielded value `" << array[index] << "` is not the same as stored value `" << value << "`");
-        throw_assert(array[index+1] == int{}, "default array value `" << array[index+1] << "` other than index is not equal default constructor value `" << int{} << "`");
+        throw_assert_eq(array[index], value, "yielded value is not the same as stored value");
+        throw_assert_eq(array[index + 1], int{}, "default array value other than index is not equal to default constructor value");
     }
 
     {
@@ -58,10 +58,10 @@ void test_aligned_array()
         std::string message = "test";
         array[0] = message;
         proxc::AlignedArray<std::string, align> other{std::move(array)};
-        throw_assert(other.size() == size, "new array size `" << array.size() << "`does not equal moved size `" << size << "`");
-        throw_assert(other[0] == message, "new array at 0 `" << other[0] << "` does not contain message `" << message << "`");
-        throw_assert(array.size() == 0, "moved array does not have length 0 `" << array.size() << "`");
-        throw_assert(array.get() == nullptr, "moved array is not nullptr `" << array.get() << "`");
+        throw_assert_eq(other.size(), size, "new array size does not equal moved size");
+        throw_assert_eq(other[0], message, "new array does not contain message");
+        throw_assert_eq(array.size(), 0u, "moved array does not have length 0");
+        throw_assert(array.get() == nullptr, "moved array is not nullptr");
     }
 }
 
