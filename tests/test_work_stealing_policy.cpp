1abcf4fd95d0f81fd7ae06bd12a4fb7daff757e9
edvardsp <edvardsp@github.com>
2017-03-11 15:37:40 +0100

    Created a single thread test for work stealing.

---
+++ tests/test_work_stealing_policy.cpp
@@ -0,0 +1,70 @@
+
+#include <proxc/policy/work_stealing_policy.hpp>
+
+#include <chrono>
+
+#include "setup.hpp"
+
+using PolicyType = proxc::policy::WorkStealing;
+using WorkType = proxc::policy::Context;
+using ClockType = std::chrono::steady_clock;
+
+void test_enqueuing_and_pick_next(PolicyType & policy)
+{
+    const std::size_t num_work = 1000;
+
+    {
+        auto work = policy.pick_next();
+        throw_assert(work == nullptr, "work should be nullptr");
+    }
+    
+    throw_assert( ! policy.is_ready(), "policy should not be ready" );
+    
+    policy.reserve(num_work);
+    for (std::size_t i = 0; i < num_work; ++i) {
+        policy.enqueue(new WorkType);
+    }
+
+    throw_assert(policy.is_ready(), "policy should be ready");
+
+    for (std::size_t i = 0; i < num_work; ++i) {
+        auto work = policy.pick_next();
+        throw_assert(work != nullptr, "work should not be nullptr");
+        delete work;
+    }
+
+    throw_assert( ! policy.is_ready(), "policy should not be ready");
+}
+
+void test_suspend_until(PolicyType & policy)
+{
+    using namespace std::chrono;
+    using namespace std::chrono_literals;
+
+    auto check_duration = [&policy](auto duration){
+        auto start = ClockType::now();
+        policy.suspend_until(ClockType::now() + duration);
+        auto diff = ClockType::now() - start;
+        throw_assert(diff > duration, "time suspended is not greater than duration");
+
+        auto time_over = diff - duration;
+        throw_assert(time_over < 200us, "time over suspension is greater than 200us => " << duration_cast<microseconds>(time_over).count() << "us");
+    };
+
+    check_duration(750us);
+    check_duration(1000us);
+    check_duration(1250us);
+    check_duration(1500us);
+    check_duration(1750us);
+    check_duration(2000us);
+}
+
+int main()
+{
+    proxc::policy::WorkStealing policy{ 1 };
+
+    test_enqueuing_and_pick_next(policy);
+    test_suspend_until(policy);
+    
+    return 0;
+}
