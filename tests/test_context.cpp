9054d065402c5ac12dcd281ad4919fb2e22b83de
edvardsp <edvardsp@github.com>
2017-03-15 16:05:07 +0100

    Remove context test, context is to be reimpleemnted.

--- tests/test_context.cpp
+++ tests/test_context.cpp
@@ -18,23 +18,7 @@ void printer(int x, int y)
 
 void test_context()
 {
-     auto main_ctx = Context::running();
-     auto scheduler = main_ctx->get_scheduler();
-     auto func1_ctx = Context::make_work_context(
-         printer,
-         42, 1337
-     );
-     auto func2_ctx = Context::make_work_context(
-         [](){ 
-            SafeCout::print("no arguments!");
-         }
-     );
-     scheduler->attach_work_context(func1_ctx);
-     scheduler->attach_work_context(func2_ctx);
-
-     scheduler->schedule(func1_ctx);
-     func2_ctx->resume();
-     std::cout << "done" << std::endl;
+    // TODO
 }
 
 int main()
