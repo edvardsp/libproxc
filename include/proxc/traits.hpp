cd7ab7c856dab4c5b89434e0d668ed8c92e9388d
edvardsp <edvardsp@github.com>
2017-03-16 20:08:58 +0100

    Fixed is_callable, as well as added make_bind. Might ditch make_bind.

--- include/proxc/traits.hpp
+++ include/proxc/traits.hpp
@@ -1,10 +1,13 @@
 
 #pragma once
 
+#include <tuple>
 #include <type_traits>
 #include <utility>
 
 #include <proxc/config.hpp>
+
+#include <boost/context/detail/apply.hpp>
 
 PROXC_NAMESPACE_BEGIN
 
@@ -23,12 +26,14 @@ template< typename Fn
 struct is_callable
 {
 private:
-    template<typename = decltype( std::declval< Fn >()( std::declval< Args >() ... ) )>
+    template<
+        typename = decltype( std::declval< Fn >()( std::declval< Args >() ... ) )
+    >
     static detail::two & helper(int);
     static detail::one & helper(...);
 
 public:
-    using value = typename std::integral_constant< bool, sizeof(helper<>(0)) - 1 >::value;
+    static constexpr bool value = std::integral_constant< bool, sizeof(helper(0)) - 1 >();
 };
 
 template<typename T>
@@ -36,6 +41,16 @@ typename std::decay< T >::type
 decay_copy(T && t) 
 {
     return std::forward< T >(t);
+}
+
+template<typename Fn, typename ...Args  >
+decltype(auto) make_bind(Fn && fn, Args && ... args)
+{
+    auto tpl = std::make_tuple( std::forward< Args >(args) ... );
+    return [fn  = decay_copy( std::forward< Fn >(fn) ),
+            tpl = std::move(tpl)]() -> void { 
+        boost::context::detail::apply(std::move(fn), std::move(tpl));
+    };
 }
 
 } // namespace traits
