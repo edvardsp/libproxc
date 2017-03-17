4116bf89ad3b2a2b94abbebb13a1c315d4cb055b
edvardsp <edvardsp@github.com>
2017-03-17 16:47:48 +0100

    Added test for make_work().

--- tests/test_scheduler.cpp
+++ tests/test_scheduler.cpp
@@ -1,14 +1,36 @@
 
+#include <iostream>
+#include <string>
+
 #include <proxc/config.hpp>
 
 #include <proxc/scheduler.hpp>
 
 #include "setup.hpp"
 
+void printer(std::string msg)
+{
+    std::cout << msg << std::endl;
+}
+    
+void test_scheduler_self()
+{
+    auto self = proxc::Scheduler::self();
+    BOOST_ASSERT(self != nullptr);
+    auto main_ctx = self->running();
+    BOOST_ASSERT(main_ctx != nullptr);
+}
 
+void test_make_work()
+{
+    auto self = proxc::Scheduler::self();
+    self->make_work( & printer, "Hello World!" );
+}
 
 int main()
 {
+    test_scheduler_self();
+    test_make_work();
 
     return 0;
 }
