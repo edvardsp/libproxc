2167242c220bebb7139b534bc287536ad07ff356
edvardsp <edvardsp@github.com>
2017-03-23 17:14:59 +0100

    Fixed tests for scheduler.

--- tests/test_scheduler.cpp
+++ tests/test_scheduler.cpp
@@ -17,51 +17,52 @@ using Clock = proxc::ClockType;
 
 using namespace proxc;
 
-template<typename Arg>
-void printer(Arg && arg)
-{
-    std::cout << arg << std::endl;
-}
-
-template<typename Arg, typename ... Args>
-void printer(Arg && arg, Args && ... args)
-{
-    std::cout << arg;
-    printer( std::forward< Args >(args)... );
-}
-
 void test_scheduler_self()
 {
     auto self = Scheduler::self();
     throw_assert( self != nullptr, "scheduler should not be nullptr" );
 }
 
-void test_make_work()
+void test_scheduler_running()
 {
-    auto func = [](std::string msg, ms milli) {
-        printer("Printing msg `", msg, "`, now sleeping ", milli.count(), "ms.");
-        Scheduler::self()->sleep_until( Clock::now() + milli );
-        printer("Printing msg `", msg, "`, after sleeping.");
+    auto running = Scheduler::running();
+    throw_assert( running != nullptr, "running ctx should not be nullptr" );
+    throw_assert( running->is_type( proxc::Context::Type::Main ), "running ctx should be main" );
+
+}
+
+void test_sleep_and_join()
+{
+    std::vector< std::size_t > answer = { 0, 100, 200, 300, 400, 500 };
+    std::vector< std::size_t > numbers;
+    std::vector< boost::intrusive_ptr< Context > > procs;
+
+    auto func = [&numbers](std::size_t milli) {
+        Scheduler::self()->sleep_until( Clock::now() + ms(milli) );
+        numbers.push_back(milli);
     };
-    auto p0 = Scheduler::make_work( func, "ctx0", ms(0) );
-    auto p1 = Scheduler::make_work( func, "ctx1", ms(100) );
-    auto p2 = Scheduler::make_work( func, "ctx2", ms(200) );
-    auto p3 = Scheduler::make_work( func, "ctx3", ms(300) );
-    auto self = Scheduler::self();
-    self->commit( p0.get() );
-    self->commit( p1.get() );
-    self->commit( p2.get() );
-    self->commit( p3.get() );
-    self->join( p3.get() );
-    self->join( p2.get() );
-    self->join( p1.get() );
-    self->join( p0.get() );
+
+    for (auto it = answer.rbegin(); it != answer.rend(); ++it) {
+        auto p = Scheduler::make_work( func, *it );
+        Scheduler::self()->commit( p.get() );
+        procs.push_back( std::move( p ) );
+    }
+
+    for (auto& p : procs) {
+        Scheduler::self()->join( p.get() );
+    }
+
+    throw_assert_equ( numbers.size(), answer.size(), "both vectors should be of equal size" );
+    for(std::size_t i = 0; i < numbers.size(); ++i) {
+        throw_assert_equ( numbers[i], answer[i], "numbers should be equal" );
+    }
 }
 
 int main()
 {
     test_scheduler_self();
-    test_make_work();
+    test_scheduler_running();
+    test_sleep_and_join();
 
     return 0;
 }
