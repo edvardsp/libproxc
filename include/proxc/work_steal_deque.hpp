f296282ad0de4dd2dec8262a36b1ef81992a29c7
edvardsp <edvardsp@github.com>
2017-03-28 16:47:01 +0200

    Changed deque to using the circular array in proxc::detail, as well as some further refactoring.

--- include/proxc/work_steal_deque.hpp
+++ include/proxc/work_steal_deque.hpp
@@ -1,25 +1,27 @@
 
 #pragma once
 
-#include <proxc.hpp>
+#include <proxc/config.hpp>
+
+#include <proxc/detail/circular_array.hpp>
 
 #include <atomic>
 #include <memory>
 #include <utility>
 
 PROXC_NAMESPACE_BEGIN
-
 namespace detail {
 
-std::ptrdiff_t to_signed(std::size_t x)
+constexpr std::ptrdiff_t to_signed( std::size_t x ) noexcept
 {
-    static_assert(   static_cast<std::size_t>(PTRDIFF_MAX) + 1 
-                  == static_cast<std::size_t>(PTRDIFF_MIN), 
-                  "Wrong integer wrapping behaviour");
-    if (x > static_cast<std::size_t>(PTRDIFF_MAX)) {
-            return static_cast<std::ptrdiff_t>(x - static_cast<std::size_t>(PTRDIFF_MIN)) + PTRDIFF_MIN;
+    constexpr std::size_t ptrdiff_max = static_cast< std::size_t >( PTRDIFF_MAX );
+    constexpr std::size_t ptrdiff_min = static_cast< std::size_t >( PTRDIFF_MIN );
+    static_assert( ptrdiff_max + 1 == ptrdiff_min, "Wrong unsigned integer wrapping behaviour");
+
+    if ( x > ptrdiff_max ) {
+        return static_cast< std::ptrdiff_t >( x - ptrdiff_min ) + PTRDIFF_MIN;
     } else {
-        return static_cast<std::ptrdiff_t>(x);
+        return static_cast< std::ptrdiff_t >( x );
     }
 }
 
@@ -33,169 +35,112 @@ std::ptrdiff_t to_signed(std::size_t x)
 // Correct and Efﬁcient Work-Stealing for Weak Memory Models
 // http://www.di.ens.fr/~zappa/readings/ppopp13.pdf
 template<typename T>
-class WorkStealDeque 
+class WorkStealDeque
 {
 public:
     static constexpr std::size_t default_capacity = 1024;
 
 private:
-    class CircularArray
-    {
-    private:
-        using AtomicType  = typename std::atomic<T *>;
-        using StorageType = typename std::aligned_storage_t<sizeof(AtomicType), cache_alignment>::type;
+    using ArrayType = detail::CircularArray< T * >;
 
-        std::size_t      size_;
-        StorageType *    storage_;
-
-    public:
-        explicit CircularArray(std::size_t size)
-            : size_{ size }
-            , storage_{ new StorageType[size_] }
-        {
-            // loop over entire array and instantiate a new AtomicType
-            for (std::size_t i = 0; i < size_; ++i) {
-                ::new(static_cast<void *>(std::addressof(storage_[i])))
-                    AtomicType{ nullptr };
-            }
-        }
-
-        ~CircularArray() 
-        {
-            // loop over entire array and call destructor for each element
-            for (std::size_t i = 0; i < size_; ++i) {
-                auto some_atomic = reinterpret_cast<AtomicType *>(std::addressof(storage_[i]));
-                some_atomic->~AtomicType();
-            }
-            delete [] storage_;
-        }
-
-        std::size_t size() const noexcept
-        {
-            return size_;
-        }
-
-        T * get(std::size_t index) noexcept
-        {
-            return reinterpret_cast<AtomicType *>
-                (std::addressof(storage_[index % size_]))
-                    ->load(std::memory_order_relaxed);
-        }
-
-        void put(std::size_t index, T * item) noexcept
-        {
-            reinterpret_cast<AtomicType *>
-                (std::addressof(storage_[index % size_]))
-                    ->store(item, std::memory_order_relaxed);
-        }
-
-        CircularArray* grow(std::size_t top, std::size_t bottom, std::size_t size)
-        {
-            auto new_array = new CircularArray{ size };
-            for (std::size_t i = top; i != bottom; i++) {
-                new_array->put(i, get(i));
-            }
-            return new_array;
-        }
-
-        CircularArray* grow(std::size_t top, std::size_t bottom) 
-        {
-            return grow(top, bottom, size_ * 2);
-        }
-    };
-
-    alignas(cache_alignment) std::atomic<std::size_t>        top_{ 0 };
-    alignas(cache_alignment) std::atomic<std::size_t>        bottom_{ 0 };
-    alignas(cache_alignment) std::atomic<CircularArray *>    array_;
+    alignas(cache_alignment) std::atomic< std::size_t >    top_{ 0 };
+    alignas(cache_alignment) std::atomic< std::size_t >    bottom_{ 0 };
+    alignas(cache_alignment) std::atomic< ArrayType * >    array_;
     char padding_[cacheline_length];
 
 public:
     WorkStealDeque()
-        : array_{ new CircularArray{ default_capacity } }
+        : array_{ new ArrayType{ default_capacity } }
     {}
 
-    ~WorkStealDeque()
+    ~WorkStealDeque() noexcept
     {
         delete array_.load();
     }
 
-    WorkStealDeque(WorkStealDeque const &)            = delete;
-    WorkStealDeque & operator=(WorkStealDeque const&) = delete;
+    WorkStealDeque( WorkStealDeque const & )             = delete;
+    WorkStealDeque & operator=( WorkStealDeque const & ) = delete;
 
     bool is_empty() const noexcept
     {
-        std::size_t bottom = bottom_.load(std::memory_order_relaxed);
-        std::size_t top = top_.load(std::memory_order_relaxed);
+        std::size_t bottom = bottom_.load( std::memory_order_relaxed );
+        std::size_t top = top_.load( std::memory_order_relaxed );
         return bottom <= top;
     }
 
     std::size_t capacity() const noexcept
     {
-        auto array = array_.load(std::memory_order_acquire);
+        auto array = array_.load( std::memory_order_acquire );
         return array->size();
     }
 
-    void reserve(std::size_t capacity) noexcept 
+    void reserve( std::size_t capacity ) noexcept
     {
-        auto array = array_.load(std::memory_order_relaxed);
-        if (capacity < array->size()) {
+        auto array = array_.load( std::memory_order_relaxed );
+        if ( capacity < array->size() ) {
             return;
         }
 
-        std::size_t bottom = bottom_.load(std::memory_order_relaxed);
-        std::size_t top = top_.load(std::memory_order_acquire);
+        std::size_t bottom = bottom_.load( std::memory_order_relaxed );
+        std::size_t top = top_.load( std::memory_order_acquire );
 
-        auto new_array = array->grow(top, bottom, capacity);
-        std::swap(array, new_array);
-        array_.store(array, std::memory_order_release);
+        auto new_array = array->grow( top, bottom, capacity );
+        std::swap( array, new_array );
+        array_.store( array, std::memory_order_release );
         delete new_array;
     }
 
     void push(T * item)
     {
-        std::size_t bottom = bottom_.load(std::memory_order_relaxed);
-        std::size_t top = top_.load(std::memory_order_acquire);
-        auto array = array_.load(std::memory_order_relaxed);
+        std::size_t bottom = bottom_.load( std::memory_order_relaxed );
+        std::size_t top = top_.load( std::memory_order_acquire );
+        auto array = array_.load( std::memory_order_relaxed );
 
         // Grow array if full
-        if (detail::to_signed(bottom - top) > detail::to_signed(array->size() - 1)) {
-            auto new_array = array->grow(top, bottom);
-            std::swap(array, new_array);
-            array_.store(array, std::memory_order_release);
+        if (   detail::to_signed( bottom - top ) 
+             > detail::to_signed( array->size() - 1 ) ) {
+            auto new_array = array->grow( top, bottom );
+            std::swap( array, new_array );
+            array_.store( array, std::memory_order_release );
             delete new_array;
         }
 
-        array->put(bottom, item);
-        std::atomic_thread_fence(std::memory_order_release);
-        bottom_.store(bottom + 1, std::memory_order_relaxed);
+        array->put( bottom, item );
+        std::atomic_thread_fence( std::memory_order_release );
+        bottom_.store( bottom + 1, std::memory_order_relaxed );
     }
 
     T * pop()
     {
-        std::size_t bottom = bottom_.load(std::memory_order_relaxed) - 1;
-        auto a = array_.load(std::memory_order_relaxed);
-        bottom_.store(bottom, std::memory_order_relaxed);
-        std::atomic_thread_fence(std::memory_order_seq_cst);
-        std::size_t top = top_.load(std::memory_order_relaxed);
+        std::size_t bottom = bottom_.load( std::memory_order_relaxed ) - 1;
+        auto a = array_.load( std::memory_order_relaxed );
+        bottom_.store( bottom, std::memory_order_relaxed );
+        std::atomic_thread_fence( std::memory_order_seq_cst );
+        std::size_t top = top_.load( std::memory_order_relaxed );
 
-        if (detail::to_signed(top - bottom) <= 0) { 
+        if ( detail::to_signed( top - bottom ) <= 0 ) {
             // non-empty queue
-            if (top == bottom) { 
+            if ( top == bottom ) {
                 // last item in queue
-                if (!top_.compare_exchange_strong(top, top + 1, std::memory_order_seq_cst, std::memory_order_relaxed)) {
+                if ( !top_.compare_exchange_strong(
+                        top,                        // expected
+                        top + 1,                    // desired
+                        std::memory_order_seq_cst,  // success
+                        std::memory_order_relaxed   // fail
+                    ) ) {
                     // failed race
-                    bottom_.store(bottom + 1, std::memory_order_relaxed);
+                    bottom_.store( bottom + 1, std::memory_order_relaxed );
                     return nullptr;
                 }
-                bottom_.store(bottom + 1, std::memory_order_relaxed);
+                bottom_.store( bottom + 1, std::memory_order_relaxed );
             }
             // Only fetch item when we know we got it.
             // This is so we don't release the item when we didn't get it.
-            auto item = a->get(bottom);
+            auto item = a->get( bottom );
             return item;
-        } else { 
+        } else {
             // empty-queue
-            bottom_.store(bottom + 1, std::memory_order_relaxed);
+            bottom_.store( bottom + 1, std::memory_order_relaxed );
             return nullptr;
         }
     }
@@ -203,24 +148,29 @@ public:
     T * steal()
     {
         // Loop while CAS fails.
-        while (true) {
+        while ( true ) {
             // Make sure top is read before bottom
-            std::size_t top = top_.load(std::memory_order_acquire);
-            std::atomic_thread_fence(std::memory_order_seq_cst);
-            std::size_t bottom = bottom_.load(std::memory_order_acquire);
+            std::size_t top = top_.load( std::memory_order_acquire );
+            std::atomic_thread_fence( std::memory_order_seq_cst );
+            std::size_t bottom = bottom_.load( std::memory_order_acquire );
 
             // Exit if deque is empty
-            if (bottom <= top) {
+            if ( detail::to_signed( bottom - top ) <= 0 ) {
                 return nullptr;
             }
 
-            CircularArray *array = array_.load(std::memory_order_consume);
+            auto array = array_.load( std::memory_order_consume );
 
             // Attempy to increment top
-            if (top_.compare_exchange_weak(top, top + 1, std::memory_order_seq_cst, std::memory_order_relaxed)) {
+            if ( top_.compare_exchange_weak(
+                    top,                        // expected
+                    top + 1,                    // desired
+                    std::memory_order_seq_cst,  // success
+                    std::memory_order_relaxed   // fail
+                ) ) {
                 // Only fetch item when we know we got it.
                 // This is so we dont release the item when we didn't get it.
-                return array->get(top);
+                return array->get( top );
             }
         }
     }
