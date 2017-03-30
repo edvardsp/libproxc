9f0fafa772a0b536d2a1088fe0a7096d4e61d0c1
edvardsp <edvardsp@github.com>
2017-03-30 14:20:28 +0200

    Some refactoring, as well as implementing a unit test.

--- include/proxc/detail/spsc_queue.hpp
+++ include/proxc/detail/spsc_queue.hpp
@@ -6,6 +6,8 @@
 
 #include <proxc/config.hpp>
 
+#include <boost/assert.hpp>
+
 PROXC_NAMESPACE_BEGIN
 namespace detail {
 
@@ -28,7 +30,71 @@ private:
     Node *    first_;
     Node *    tail_copy_;
 
-    Node * alloc_node()
+public:
+    SpscQueue()
+    {
+        auto n = new Node{};
+        tail_.store( n, std::memory_order_relaxed );
+        head_ = first_ = tail_copy_ = n;
+    }
+
+    ~SpscQueue() noexcept
+    {
+        BOOST_ASSERT( first_ != nullptr );
+        auto n = first_;
+        do {
+            auto next = n->next_.load( std::memory_order_relaxed );
+            delete n;
+            n = next;
+        } while ( n != nullptr );
+    }
+
+    // make non-copyable
+    SpscQueue( SpscQueue const & ) = delete;
+    SpscQueue & operator = ( SpscQueue const & ) = delete;
+
+    bool is_empty() const noexcept
+    {
+         auto tail = tail_.load( std::memory_order_consume );
+         return tail->next_.load( std::memory_order_acquire ) == nullptr;
+    }
+
+    void enqueue( ItemType const & item ) noexcept
+    {
+        auto n = alloc_node_();
+        n->item_ = item;
+        enqueue_impl_( n );
+    }
+
+    void enqueue( ItemType && item ) noexcept
+    {
+        auto n = alloc_node_();
+        n->item_ = std::forward< ItemType >( item );
+        enqueue_impl_( n );
+    }
+
+    bool dequeue( ItemType & item ) noexcept
+    {
+        auto tail = tail_.load( std::memory_order_consume );
+        auto next = tail->next_.load( std::memory_order_acquire );
+        if ( next != nullptr ) {
+            item = next->item_;
+            tail_.store( next, std::memory_order_release );
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+private:
+    void enqueue_impl_( Node * n ) noexcept
+    {
+        BOOST_ASSERT( n != nullptr );
+        head_->next_.store( n, std::memory_order_release );
+        head_ = n;
+    }
+
+    Node * alloc_node_()
     {
         if ( first_ != tail_copy_ ) {
             auto n = first_;
@@ -46,65 +112,6 @@ private:
         return new Node{};
     }
 
-public:
-    SpscQueue()
-    {
-        auto n = new Node{};
-        tail_.store( n, std::memory_order_relaxed );
-        head_ = first_ = tail_copy_ = n;
-    }
-
-    ~SpscQueue() noexcept
-    {
-        auto n = first_;
-        do {
-            auto next = n->next_.load( std::memory_order_relaxed );
-            delete n;
-            n = next;
-        } while ( n != nullptr );
-    }
-
-    SpscQueue( SpscQueue const & ) = delete;
-    SpscQueue & operator = ( SpscQueue const & ) = delete;
-
-    bool is_empty() const noexcept
-    {
-         auto tail = tail_.load( std::memory_order_consume );
-         return tail->next_.load( std::memory_order_acquire ) == nullptr;
-    }
-
-    void enqueue_impl( Node * n ) noexcept
-    {
-        head_->next_.store( n, std::memory_order_release );
-        head_ = n;
-    }
-
-    void enqueue( ItemType const & item ) noexcept
-    {
-        auto n = alloc_node();
-        n->item_ = item;
-        enqueue_impl( n );
-    }
-
-    void enqueue( ItemType && item ) noexcept
-    {
-        auto n = alloc_node();
-        n->item_ = std::forward< ItemType >( item );
-        enqueue_impl( n );
-    }
-
-    bool dequeue( ItemType & item ) noexcept
-    {
-        auto tail = tail_.load( std::memory_order_consume );
-        auto next = tail->next_.load( std::memory_order_acquire );
-        if ( next != nullptr ) {
-            item = next->item_;
-            tail_.store( next, std::memory_order_release );
-            return true;
-        } else {
-            return false;
-        }
-    }
 };
 
 } // namespace detail
