79b7b55379b031036d8c0462dc808f9dd8b01003
edvardsp <edvardsp@github.com>
2017-04-04 16:56:41 +0200

    Decided to only have a single parallel method, which takes a number of processes to run in parallel. This now supports the API of a single proc /proc()/ and replicated proc /proc_for()/.

--- include/proxc/parallel.hpp
+++ include/proxc/parallel.hpp
@@ -1,11 +1,86 @@
 
 #pragma once
 
+#include <iostream>
+
+#include <array>
+#include <iterator>
+#include <type_traits>
+#include <utility>
+
 #include <proxc/config.hpp>
 
+#include <proxc/context.hpp>
+#include <proxc/process.hpp>
+#include <proxc/scheduler.hpp>
+
+#include <boost/intrusive_ptr.hpp>
+#include <boost/range/irange.hpp>
+
 PROXC_NAMESPACE_BEGIN
+namespace detail {
 
+template<typename P>
+struct is_proc
+    : std::integral_constant<
+        bool,
+        std::is_same<
+            typename std::decay< P >::type,
+            Process
+        >::value
+    >
+{};
 
+template<typename ...>
+struct are_procs
+    : std::true_type
+{};
+
+template<typename Proc, typename ... Procs>
+struct are_procs< Proc, Procs ... >
+    : std::integral_constant<
+        bool,
+           is_proc< Proc >::value
+        && are_procs< Procs ... >{}
+    >
+{};
+
+template<typename Proc>
+auto parallel_impl( Proc && proc )
+    -> std::enable_if_t<
+           is_proc< Proc >::value
+        && std::is_move_assignable< Proc >::value
+    >
+{
+    Proc p = std::forward< Proc >( proc );
+    p.launch();
+    p.join();
+}
+
+template<typename Proc, typename ... Procs>
+auto parallel_impl(Proc && proc, Procs && ... procs )
+    -> std::enable_if_t<
+           is_proc< Proc >::value
+        && std::is_move_assignable< Proc >::value
+    >
+{
+    Proc p = std::forward< Proc >( proc );
+    p.launch();
+    parallel_impl( std::forward< Procs >( procs ) ... );
+    p.join();
+}
+
+} // namespace detail
+
+template<typename ... Procs>
+void parallel(Procs && ... procs)
+{
+    static_assert( detail::are_procs< Procs ... >::value,
+        "Supplied arguments must be of type proxc::Process" );
+    static_assert( traits::are_move_assigneable< Procs ... >::value,
+        "Supplied processes must be move assigneable" );
+    detail::parallel_impl( std::forward< Procs >( procs ) ... );
+}
 
 PROXC_NAMESPACE_END
 
--- tests/test_parallel.cpp
+++ tests/test_parallel.cpp
@@ -1,35 +1,109 @@
 
+#include <chrono>
+#include <iostream>
+#include <vector>
+
 #include <proxc/config.hpp>
 
 #include <proxc/parallel.hpp>
+#include <proxc/process.hpp>
+#include <proxc/channel/sync.hpp>
+#include <proxc/channel/async.hpp>
+#include <proxc/channel/op.hpp>
 
 #include "setup.hpp"
 
 using namespace proxc;
+namespace chan = channel::async;
+template<typename T> using Tx = chan::Tx< T >;
+template<typename T> using Rx = chan::Rx< T >;
+
+void fn1()
+{
+    std::cout << "fn1" << std::endl;
+}
+
+void fn2()
+{
+    std::cout << "fn2" << std::endl;
+}
+
+void fn3()
+{
+    std::cout << "fn3" << std::endl;
+}
+
+void fn4( int i )
+{
+    std::cout << "fn4: " << i << std::endl;
+}
+
+void fn5( int x, int y )
+{
+    std::cout << "fn5: " << x << ", " << y << std::endl;
+}
+
+void generate( Tx< int > out, Rx< int > ex )
+{
+    int i = 2;
+    while ( ! ex.is_closed() ) {
+        out.send( i++ );
+    }
+}
+
+void filter( Rx< int > in, Tx< int > out )
+{
+    int prime;
+    in.recv( prime );
+    for ( auto i : in ) {
+        if ( i % prime != 0 ) {
+            out.send( i );
+        }
+    }
+}
+
+void benchmark( Rx< int > in, Tx< int > ex )
+{
+
+}
 
 int main()
 {
+    const int n = 400;
+    auto chans = chan::create_n< int >( n );
+    auto ex_ch = chan::create< int >();
+    /* auto iota = iota_n( n-1 ); */
+
+    auto start = std::chrono::steady_clock::now();
     parallel(
-        make_proc( 1 ),
-        make_proc( 2 ),
-        make_proc( 3 ),
-        make_proc_list(
-            make_proc( 4 ), 
-            make_proc( 5 ), 
-            make_proc( 6 ) 
+        proc( generate, channel::get_tx( chans[0] ), channel::get_rx( ex_ch ) ),
+        proc_for( 0, n-1,
+            [&chans]( auto i ) {
+                filter( channel::get_rx( chans[i] ), channel::get_tx( chans[i+1] ) );
+            }
         ),
-        make_proc_list(
-            make_proc( 7 ), 
-            make_proc_list(
-                make_proc( 8 ),
-                make_proc( 9 )
-            ), 
-            make_proc( 10 ) 
-        ),
-        make_proc( 11 ),
-        make_proc_list_for< 3 >(
-            make_proc( 12 ),
-            make_proc( 13 )
+        proc(
+            [start,n]( Rx< int > in, Tx< int > ex ){
+                int prime;
+                in.recv( prime );
+                auto end = std::chrono::steady_clock::now();
+                std::chrono::duration< double, std::micro > diff = end - start;
+                std::cout << n << "th prime is " << prime << std::endl;
+                std::cout << "each prime took approx " << diff.count() / n << "us" << std::endl;
+                ex.close();
+            },
+            channel::get_rx( chans[n-1] ), channel::get_tx( ex_ch )
         )
     );
+
+    /* Process p{ fn3 }; */
+    /* parallel( */
+    /*     Process{ fn1 } */
+    /* ); */
+    /* parallel( */
+    /*     Process{ fn1 }, */
+    /*     Process{ fn2 }, */
+    /*     Process{ fn3 }, */
+    /*     std::move( p ) */
+    /* ); */
 }
