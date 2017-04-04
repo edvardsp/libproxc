82587686a16fd84aad418e606348008cf0c32f54
edvardsp <edvardsp@github.com>
2017-04-04 16:54:13 +0200

    Added more traits, mostly for implementation of parallel and processes.

--- include/proxc/traits.hpp
+++ include/proxc/traits.hpp
@@ -33,6 +33,43 @@ struct is_callable
     : detail::is_callable_impl< Expr >
 {};
 
+template<typename Arg, typename ...>
+struct are_callable_with_arg
+    : std::true_type
+{};
+
+template<typename Arg, typename Fn, typename ... Fns>
+struct are_callable_with_arg< Arg, Fn, Fns ... >
+    : std::integral_constant< bool,
+           is_callable< Fn(Arg) >{}
+        && are_callable_with_arg< Arg, Fns ... >{}
+    >
+{};
+
+template<typename ...>
+struct are_move_assigneable
+    : std::true_type
+{};
+
+template<typename Proc, typename ... Procs>
+struct are_move_assigneable< Proc, Procs ... >
+    : std::integral_constant< bool,
+           std::is_move_assignable< Proc >::value
+        && are_move_assigneable< Procs ... >{}
+    >
+{};
+
+template<typename It, typename = void>
+struct is_inputiterator
+    : std::false_type
+{};
+
+template<typename It>
+struct is_inputiterator< It, decltype( *std::declval< It & >(), ++std::declval< It & >(), void() )>
+    : std::true_type
+{};
+
+
 template<typename T>
 typename std::decay< T >::type
 decay_copy(T && t)
